08111be036cc31042b9f7d47859d7a76e6dadd52
carl <karol-nowakowski@o2.pl>
2019-11-01 12:23:42 +0100

    feat: final version 0.1

--- src/js/main.js
+++ src/js/main.js
@@ -20,14 +20,38 @@ if ('serviceWorker' in navigator) {
 const buttonAdd = document.querySelector('.button-add--js');
 const buttonRemove = document.querySelector('.button-remove--js');
 const value = document.querySelector('.counter__value--js');
+const key = new Date().toISOString().slice(0, 10);
+console.log(key);
+
+// ! - wykrzyknik zaprzecza
+if (!localStorage.getItem(key)) {
+  localStorage.setItem(key, 0);
+  value.innerHTML= '0';
+
+}
+else { value.innerHTML= '0';
+value.innerHTML = localStorage.getItem(key);
+
+}
+// stary wpis działajacy na wartosciach HTML
+//   buttonAdd.addEventListener('click', (e) => {
+ //  value.innerHTML = parseInt(value.innerHTML) + 1;
+//   });
 
 buttonAdd.addEventListener('click', (e) => {
-  value.innerHTML = parseInt(value.innerHTML) + 1;
+  localStorage.setItem(key,parseInt( localStorage.getItem(key)) + 1);
+  value.innerHTML = localStorage.getItem(key) ;
 });
+// Stary wpis działajacy na HTML
+// buttonRemove.addEventListener('click', (e) => {
+//  const currentValue = parseInt(value.innerHTML);
+//  if (currentValue > 0) {
+//  value.innerHTML = currentValue - 1; }
+// });
 
 buttonRemove.addEventListener('click', (e) => {
-  const currentValue = parseInt(value.innerHTML);
+  const currentValue = parseInt(localStorage.getItem(key));
   if (currentValue > 0) {
-
-  value.innerHTML = currentValue - 1; }
+  localStorage.setItem(key, localStorage.getItem(key) - 1);
+  value.innerHTML = localStorage.getItem(key) }
 });
